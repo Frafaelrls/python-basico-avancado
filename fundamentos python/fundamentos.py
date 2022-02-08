305eb84c02d2c6dcb8ad8759b146c5613f5d4f57
Felippe <frafaelrls@gmail.com>
2022-02-08 11:39:40 -0300

    Adding funcamento.py

---
+++ fundamentos python/fundamentos.py
@@ -0,0 +1,25 @@
+"""Sentença de código
+- Deve-se respeitar a indentação como as quebras de linhas dos comandos e
+padrões de programação predefinidas.
+- Deve-se ficar atento ao espaçamento durante o uso de comandos.
+- É comumente usado uma sentença por linha
+- Recomenda-se respeitar o limite de 79 carácteres por linha conforme a pep 8
+"""
+print('Hello World')
+print(True)  # Tipo boolean, colocar com letra maiúscula
+print(2 + 3)  # Operação aritmética
+print(3 * ' muito')  # O texto será repetido três vezes
+print('Concatenar o' + 2 * 'texto')  # concatenação do texto usando '+'
+# print(2 + 'muito')  # Em caso de ambiguidade o programa não será rodado
+
+"""Variáveis
+- Em python não é necessário definir o tipo da variável o interpretador fará 
+essa função.
+- Atribuição de variáveis deve-se ter o identificador, o simbolo de atribuição
+e o valor.
+"""
+
+a = 10.5  # tipo flout
+b = 5  # tipo int
+c = 'Texto'  # tipo string
+print(a + b)

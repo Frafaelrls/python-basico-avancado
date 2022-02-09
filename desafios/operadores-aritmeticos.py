267e27928fcd2e01a096a74eb116668153e47b6c
Felippe <frafaelrls@gmail.com>
2022-02-09 10:39:43 -0300

    update operadores-aritimetivos

--- desafios/operadores-aritmeticos.py
+++ desafios/operadores-aritmeticos.py
@@ -7,7 +7,12 @@ um usuário.
 Desafio proposto inicialmente recebendo valores pré-definidos nas variáveis.
 
 """
-salario = None
-gasto = None
 
+salario = float(input(f"Informe o seu salário\n"))
+gasto = float(input(f"Informe o seu gasto\n"))
 
+porcentagem = gasto / salario
+# round(x,y) função responsável por arredondar o a quantidade de casas decimais
+# print(f"O seu gasto corresponde a {round(porcentagem *100,2)}%")
+# Segunda forma que pode-se limitar
+print(f"O seu gasto corresponde a {'{:.2f}'.format(porcentagem * 100)}")

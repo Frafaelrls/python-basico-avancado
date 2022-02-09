173da69eafb75900560db72fe468c40dcc17e082
Felippe <frafaelrls@gmail.com>
2022-02-09 09:43:00 -0300

    Adding 'operadore-aritmeticos'

---
+++ desafios/operadores-aritmeticos.py
@@ -0,0 +1,13 @@
+"""
+Desafio proposto:
+
+Criar um programa que irá calcular o percentual de gastos de um salário de
+um usuário.
+
+Desafio proposto inicialmente recebendo valores pré-definidos nas variáveis.
+
+"""
+salario = None
+gasto = None
+
+

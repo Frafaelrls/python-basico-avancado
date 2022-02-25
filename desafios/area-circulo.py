f3794ee310a2c7f2120cced4fd75789d9e9fd35c
Felippe <frafaelrls@gmail.com>
2022-02-25 11:19:08 -0300

    creating returnable function

--- desafios/area-circulo.py
+++ desafios/area-circulo.py
@@ -4,14 +4,14 @@ import os
 
 def circulo(raio):
     pi = math.pi
-    area = pi * raio ** 2
-    area = format(area, '.2f')
-    print(f'A area do circulo é {area}cm²')
-    print('Aperte qualquer tecla para sair.')
-    os.system('pause > nul')
+    return pi * raio ** 2
 
 
 if __name__ == '__main__':
     entrada = input('Insira o raio do circulo em cm\n')
     entrada = float(entrada)
-    circulo(entrada)
+    area = circulo(entrada)
+    area = format(area, '.2f')
+    print(f'A area do circulo é {area}cm²')
+    print('Aperte qualquer tecla para sair.')
+    os.system('pause > nul')

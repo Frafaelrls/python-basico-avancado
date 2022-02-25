32f28726c8a5bc544767999fc4a7b239cef128c5
Felippe <frafaelrls@gmail.com>
2022-02-25 11:45:33 -0300

    adding argument validation

--- desafios/area-circulo.py
+++ desafios/area-circulo.py
@@ -9,10 +9,15 @@ def circulo(raio):
 
 
 if __name__ == '__main__':
-    entrada = sys.argv[1]
-    entrada = float(entrada)
-    area = circulo(entrada)
-    area = format(area, '.2f')
-    print(f'A area do circulo é {area}cm²')
-    print('Aperte qualquer tecla para sair.')
-    os.system('pause > nul')
+    if len(sys.argv) < 2:
+        print(f"É necessário informar o raio do circulo\n"
+              f"syntax: {sys.argv[0]} <raio>")
+        os.system('pause')
+    else:
+        entrada = sys.argv[1]
+        entrada = float(entrada)
+        area = circulo(entrada)
+        area = format(area, '.2f')
+        print(f'A area do circulo é {area}cm²')
+        print('Aperte qualquer tecla para sair.')
+        os.system('pause > nul')

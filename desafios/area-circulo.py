52634f52e437feb010443a3ec5701fdcea4825ea
Felippe <frafaelrls@gmail.com>
2022-02-25 10:56:39 -0300

    creating a function for the calculation

--- desafios/area-circulo.py
+++ desafios/area-circulo.py
@@ -1,13 +1,17 @@
 import math
 import os
 
-pi = math.pi
 
-raio = input('Insira o raio do circulo em cm\n')
-raio = float(raio)
+def circulo(raio):
+    pi = math.pi
+    area = pi * raio ** 2
+    area = format(area, '.2f')
+    print(f'A area do circulo é {area}cm²')
+    print('Aperte qualquer tecla para sair.')
+    os.system('pause > nul')
 
-area = pi * raio**2
-area = format(area, '.2f')
-print(f'A area do circulo é {area}cm²')
-print('Aperte qualquer tecla para sair.')
-os.system('pause > nul')
+
+if __name__ == '__main__':
+    entrada = input('Insira o raio do circulo em cm\n')
+    entrada = float(entrada)
+    circulo(entrada)

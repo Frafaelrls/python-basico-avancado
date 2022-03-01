c91319360e2700c8deba3467de613a7156427c23
Frafaelrls <frafaelrls@gmail.com>
2022-03-01 14:43:45 -0300

    adding loop function

--- desafios/conceitos-alunos.py
+++ desafios/conceitos-alunos.py
@@ -14,42 +14,46 @@ E           De 1,1 a 2,0
 E-          De 0,0 a 1,0
 
 """
+while True:
+    try:
+        nota = input('Por favor informe a nota do aluno.\n')
+        nota = float(nota)
 
-nota = input('Por favor informe a nota do aluno.\n')
-nota = float(nota)
+        if 9.1 <= nota <= 10.0:
+            print('O aluno está com o conceito: A')
 
-if 9.1 <= nota <= 10.0:
-    print('O aluno está com o conceito: A')
+        elif 8.1 <= nota <= 9.0:
+            print('O aluno está com o conceito: A-')
 
-if 8.1 <= nota <= 9.0:
-    print('O aluno está com o conceito: A-')
+        elif 7.1 <= nota <= 8.0:
+            print('O aluno está com o conceito: B')
 
-if 7.1 <= nota <= 8.0:
-    print('O aluno está com o conceito: B')
+        elif 6.1 <= nota <= 7.0:
+            print('O aluno está com o conceito: B-')
 
-if 6.1 <= nota <= 7.0:
-    print('O aluno está com o conceito: B-')
+        elif 5.1 <= nota <= 6.0:
+            print('O aluno está com o conceito: C')
 
-if 5.1 <= nota <= 6.0:
-    print('O aluno está com o conceito: C')
+        elif 4.1 <= nota <= 5.0:
+            print('O aluno está com o conceito: C-')
 
-if 4.1 <= nota <= 5.0:
-    print('O aluno está com o conceito: C-')
+        elif 3.1 <= nota <= 4.0:
+            print('O aluno está com o conceito: D')
 
-if 3.1 <= nota <= 4.0:
-    print('O aluno está com o conceito: D')
+        elif 2.1 <= nota <= 3.0:
+            print('O aluno está com o conceito: D-')
 
-if 2.1 <= nota <= 3.0:
-    print('O aluno está com o conceito: D-')
+        elif 1.1 <= nota <= 2.0:
+            print('O aluno está com o conceito: E')
 
-if 1.1 <= nota <= 2.0:
-    print('O aluno está com o conceito: E')
+        elif 0.0 <= nota <= 1.0:
+            print('O aluno está com o conceito: E-')
 
-if 0.0 <= nota <= 1.0:
-    print('O aluno está com o conceito: E-')
+        elif nota >= 10.1:
+            print('Nota inválida')
 
-if nota >= 10.1:
-    print('Nota inválida')
-
-if nota <= -0.9:
-    print('Nota Inválida')
+        else:
+            print('Nota Inválida')
+    except ValueError:
+        print('Erro')
+        break

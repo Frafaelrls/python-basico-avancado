7556a9b88f0242ea54a5d716fa840f41af28ba44
Frafaelrls <frafaelrls@gmail.com>
2022-08-28 12:45:27 -0300

    adicionando arquivo io_v3.py, manipulação de arquivos, usando laço for com função strip

--- manipulacao_de_arquivos/io_v3.py
+++ manipulacao_de_arquivos/io_v3.py
@@ -1,5 +1,5 @@
 arquivo = open('pessoas.csv')
 for registro in arquivo:
-    print('Nome: {} Idade {}'.format(*registro.split(',')))
+    print('Nome: {} Idade {}'.format(*registro.strip().split(',')))
 arquivo.close() 
  
